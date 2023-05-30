3a70d6e6c4d4389e8116b5b529c76c2f6f621182
donnie <donnie.lee@pospot.kr>
2023-05-30 15:54:12 +0900

    zendesk API history 목록 호출완료

--- src/views/TicketHistory.js
+++ src/views/TicketHistory.js
@@ -1,26 +1,25 @@
-import React, { useEffect } from 'react';
+import React, { useEffect,useState } from 'react';
 import axios from 'axios';
 
 function TicketHistory() {
-  useEffect(() => {
-    const fetchTicketHistory = async () => {
-      try {
-        const response = await axios.post('https://pospot.zendesk.com/api/v2/ticket_audits.json', {
-          headers: {
-            Authorization: `Bearer ${process.env.REACT_APP_ZENDESK_API_KEY}`,
-            'Content-Type': 'application/json'
-          }
-        });
-        console.log(response.data);
-        // 여기서 히스토리 데이터를 처리하거나 상태에 저장할 수 있습니다.
-      } catch (error) {
-        console.error(error);
-      }
-    };
 
-    fetchTicketHistory();
+  const [tickets, setTickets] = useState([]);
+
+  useEffect(() => {
+    axios.get(process.env.REACT_APP_ZENDESK_URL, {
+      headers: {
+        "Authorization": `Bearer ${process.env.REACT_APP_ZENDESK_API_KEY}`,
+        'Access-Control-Allow-Origin' : process.env.REACT_APP_FRONT_URL,
+        'withCredentials':true
+      }
+    }).then((response) => {
+      setTickets(response.data);
+    }).catch((err) => {
+      console.log('error',err)
+    });
   }, []);
 
+  
   return (
     <div>
       <h1>Ticket History</h1>
