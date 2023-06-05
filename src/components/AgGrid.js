7e96e9b46c35ca905ed53868da1a8eba36d8a55e
ez <bene29@pospot.kr>
2023-06-05 19:17:02 +0900

    main 진행 중

---
+++ src/components/AgGrid.js
@@ -0,0 +1,108 @@
+import React, { useState, useRef, useEffect, useMemo, useCallback, useLayoutEffect } from 'react';
+import { AgGridReact } from 'ag-grid-react'; // the AG Grid React Component
+import Pagination from "react-js-pagination";
+
+import 'ag-grid-community/styles/ag-grid.css'; // Core grid CSS, always needed
+import 'ag-grid-community/styles/ag-theme-alpine.css'; // Optional theme CSS
+import 'ag-grid-enterprise';
+
+/**
+ *  작성자 : 이은정
+ *  작성일 : 2023.05.22
+ *  기능 : 배열전달시 엑셀로 추출
+ * @param {
+ * data : 행 데이터
+ * column : 컬럼명
+ * paging : 페이징 여부 true/false
+ * }
+ * @returns
+*/
+const AgGrid = ({data, column, paging}) => {
+    
+    const gridRef = useRef(); // Optional - for accessing Grid's API
+    const [rowData, setRowData] = useState(); // Set rowData to Array of Objects, one Object per Row
+    const [columnDefs, setColumnDefs] = useState();
+
+    /** 페이징 관련 ▼ ============================================================= */
+    const [activePage, setActivePage] = useState(1); // 현재 페이지
+    const [itemsPerPage] = useState(10); // 페이지당 아이템 갯수
+
+    const setPage = (e) => {
+        setActivePage(e);
+        console.log('page ---->', e);
+        gridRef.current.api.paginationGoToPage(e);
+    };
+    /** 페이징 관련 ▲ ============================================================= */
+
+    // 데이터, 컬럼명 설정
+    useLayoutEffect(() => {
+        setRowData(data)
+        setColumnDefs(column)
+        // setColumnDefs(column.map(col => ({
+        //     field: col, filter: true
+        // })))
+    }, [data]);
+
+    const onGridReady = useCallback((params) => {
+        // setRowData(data)
+        // setColumnDefs(column)
+    }, []);
+
+    // DefaultColDef sets props common to all Columns
+    const defaultColDef = useMemo(()=> ({
+        sortable: true
+    }));
+
+    // Example > 그리드 클릭 시 row값 콘솔 출력
+    const cellClickedListener = useCallback( e => {
+        console.log('cellClicked', e.data);
+    }, []);
+
+    // Example > 선택한 row 해제
+    const buttonListener = useCallback( e => {
+        gridRef.current.api.deselectAll();
+    }, []);
+
+    return (
+        <div>
+          {
+            data && (
+            <>
+            {/* On div wrapping Grid a) specify theme CSS Class Class and b) sets Grid size */}
+            {/* ag-grid */}
+            <div className="ag-theme-alpine" style={{height: 500}}>
+                <AgGridReact 
+                    ref={gridRef} // Ref for accessing Grid's API 
+                    rowData={rowData} // Row Data for Rows 
+                    columnDefs={columnDefs} // Column Defs for Columns 
+                    defaultColDef={defaultColDef} // Default Column Properties 
+                    animateRows={true} // Optional - set to 'true' to have rows animate when sorted
+                    rowSelection='multiple' // Options - allows click selection of rows 
+                    onCellClicked={cellClickedListener} // Optional - registering for Grid Event 
+                    pagination={true}
+                    paginationPageSize={10}
+                    suppressPaginationPanel={true}
+                    suppressScrollOnNewData={true}
+                    onGridReady={onGridReady}
+                />
+            </div>
+            {/* react-js-pagination */}
+            {paging ?
+            <Pagination
+                activePage={activePage} // 현재 페이지
+                itemsCountPerPage={itemsPerPage} // 한 페이지 당 보여줄 아이템 수
+                totalItemsCount={data?.length} // 총 아이템 수
+                pageRangeDisplayed={5} // paginator의 페이지 범위
+                prevPageText={"‹"} // "이전"을 나타낼 텍스트
+                nextPageText={"›"} // "다음"을 나타낼 텍스트
+                onChange={setPage} // 페이지 변경을 핸들링하는 함수
+            />
+            : null
+            }
+            </>
+          )}
+        </div>
+      );
+};
+
+export default AgGrid;
