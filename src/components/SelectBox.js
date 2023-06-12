53a54bfea49206de05a1083a71d1e49e148f4203
donnie <donnie.lee@pospot.kr>
2023-06-12 16:38:40 +0900

    select box 테마 색상 변경

--- src/components/SelectBox.js
+++ src/components/SelectBox.js
@@ -5,11 +5,37 @@ const SelectBox = (props) => {
     
     const {options, handleChange} = props;
 
+    const customStyles = {
+        control: (provided, state) => ({
+            ...provided,
+            borderColor: state.isFocused ? '#BB0841' : 'grey',
+            boxShadow: state.isFocused ? '0 0 0 2px rgba(255, 0, 0, 0.3)' : null,
+            backgroundColor: state.isSelected ? "red" : "white",
+            color: state.isSelected ? "white" : "black",
+            '&:hover': {
+                borderColor: '#BB0841',
+                backgroundColor: 'white'
+              },
+            '&:focus': {
+                borderColor: '#BB0841'
+              },
+            '&:selected': {
+                backgroundColor: 'black'
+            },
+        }),
+        option: (provided, state) => ({
+            ...provided,
+            backgroundColor: state.isSelected ? '#BB0841' : 'white',
+          }),
+    };
+
     return (
-        <Select
-        className="custom-select"
+        <Select 
+            className="custom-select"
             options={options}
             onChange={handleChange}
+            styles={customStyles}
+            
         />
     )
 }
