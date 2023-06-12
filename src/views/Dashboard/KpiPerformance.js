9f47a90998acd02d105f06bbe4fc5bbcd0738758
ez <bene29@pospot.kr>
2023-06-12 19:20:31 +0900

    kpi performance 진행 중

--- src/views/Dashboard/KpiPerformance.js
+++ src/views/Dashboard/KpiPerformance.js
@@ -1,13 +1,23 @@
-import { useContext, useState } from "react"
-import { TestContext } from "../../hooks/TestContext"
+import { useState, useEffect } from 'react';
+import LineChart from "../../components/Chart";
+import AgGrid from "../../components/AgGrid";
+import Header from '../../components/Header';
+import Top from '../../components/Top';
+
+import '../../scss/style.scss';
 
 function KpiPerformance() {
 
-    // const testValue = useContext(TestContext)
     return (
-        <>
-       <div className="test">KpiPerformance</div>
-        </>
+        <div className="kpi-container">
+            <Header />
+            <div className='inner-container'>
+                <Top auth={false} />
+                <div className='kpi-nav'>
+                    
+                </div>
+            </div>
+        </div>
     )
 }
 
