ec2af02aba5e91b1b6df56998212fcb96b2d13cf
ez <bene29@pospot.kr>
2023-07-12 10:30:54 +0900

    react-select 높이 수정

--- src/components/SelectBox.js
+++ src/components/SelectBox.js
@@ -3,18 +3,22 @@ import Select from 'react-select';
 
 const SelectBox = (props) => {
     
-    const {options, handleChange, defaultValue, placeholder} = props;
+    const {options, handleChange, defaultValue, placeholder, minHeight} = props;
 
     const customStyles = {
         control: (provided, state) => ({
             ...provided,
-            borderColor:  state.isSelected ? '#BB0841' : state.isFocused ? '#BB0841' : 'grey',
+            borderColor:  state.isSelected ? '#BB0841' : state.isFocused ? '#BB0841' : '#D8D8D8',
             boxShadow: state.isFocused ? '0 0 0 2px rgba(255, 0, 0, 0.3)' : null,
+            minHeight: '32px',
+            height: minHeight ? '32px' : null,
         }),
         option: (provided, state) => ({
             ...provided,
-            borderColor: state.isSelected ? '#BB0841' : state.isFocused ? '#BB0841' : 'grey',
+            borderColor: state.isSelected ? '#BB0841' : state.isFocused ? '#BB0841' : '#D8D8D8',
             backgroundColor: state.isSelected ? '#BB0841' : state.isFocused ? '#FAF1F4' : 'white',
+            minHeight: '32px',
+            height: minHeight ? '32px' : null,
           }),
     };
 
